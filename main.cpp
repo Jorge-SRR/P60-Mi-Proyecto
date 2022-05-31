d0a39d433daed5805ee85be795af3ebc787ab9cc
Santiago Brito <davidbri89@outlook.com>
2022-05-31 10:30:54 -0500

    Agregar una clase persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "persona.h"
 using namespace std;
 
 int main()
 {
     cout << "Mi primer proyecto en git y github" << endl;
+    Persona P;
+    P.nombre = "David Brito";
+    P.edad = 21;
+    cout <<P.nombre<<endl;
+    cout <<P.edad <<endl;
     return 0;
 }
